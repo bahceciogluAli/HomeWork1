96e60a889ada54ffb360a5de5c45ba4daea052a5
alibahcecioglu <alibahcecioglu@hotmail.com>
2021-12-26 02:18:40 +0300

    v3

--- demo/src/main/java/com/example/homework_1/Parallelism.java
+++
@@ -1,9 +0,0 @@
-package com.example.homework_1;
-
-public class Parallelism {
-    int sumOfWeights = widgets.parallelStream()
-            .filter(b -> b.getColor() == RED)
-            .mapToInt(b -> b.getWeight())
-            .sum();
-
-}
